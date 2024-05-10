65e635fc0cf5c0d15b5d7d8a9741279efffe2287
victor <vmblanco.20@est.ucab.edu.ve>
2024-05-10 13:49:06 +0000

    Arreglando el string de conexion

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,16 @@ import { Module } from '@nestjs/common';
 import { RamenModule } from './ramen/ramen.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
-
 @Module({
   imports: [
-    
-    MongooseModule.forRoot('mongodb+srv://white:irma0403@cluster0.zk3yt4e.mongodb.net/ramen-db'),
+    // MongooseModule.forRoot('mongodb+srv://white:irma0403@cluster0.zk3yt4e.mongodb.net/ramen-db'),
 
-    RamenModule],
+    MongooseModule.forRoot(
+      'mongodb://10.18.208.157:27017,10.18.208.158:27017,10.18.208.212:27017/ramen-db?replicaSet=myReplicaSet',
+    ),
+
+    RamenModule,
+  ],
   controllers: [],
   providers: [],
 })
