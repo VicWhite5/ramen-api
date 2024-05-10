3bdc9cddac3e83a413e562634d21b7737fba0473
victor <vmblanco.20@est.ucab.edu.ve>
2024-05-10 21:21:12 +0000

    Terminando el string de conexion. Esperemos sea el ultimo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
 import { RamenModule } from './ramen/ramen.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    // MongooseModule.forRoot('mongodb+srv://white:irma0403@cluster0.zk3yt4e.mongodb.net/ramen-db'),
+
+    ConfigModule.forRoot(),
 
     MongooseModule.forRoot(
-      'mongodb://10.18.208.157:27017,10.18.208.158:27017,10.18.208.212:27017/ramen-db?replicaSet=myReplicaSet',
+      `mongodb://${process.env.DB_HOSTS}/${process.env.DB_NAME}?replicaSet=${process.env.REPLICA_SET}`
     ),
 
     RamenModule,
